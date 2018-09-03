28714228cc7432f0c3e54a56a8273b8a15b80174
Francisco Rafart <francisco.rafart@gmail.com>
2018-09-02 22:29:44 -0400

    change id for class

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     return (
       <div>
           <Header/>
-          <Body id="container" className="clearfix"/>
+          <Body className="container clearfix"/>
           <Footer/>
       </div>
     );
